98ba72ecfdb9fb6750b56000f9743d5f647710b2
Bhim pd Lamichhane <51348717+bhimpd@users.noreply.github.com>
2024-01-06 08:13:11 +0545

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+
+# check if directory path is provided as an argument
+
+if [ "$#" -ne 1 ]; then
+echo "Usage:$0 ./shellscript"
+exit 1
+fi
+
+# Input directory to backup
+
+source_directory="$1"
+
+# Backup directory
+backup_directory="backup"
+mkdir -p "$backup_directory"
+
+#Timestamp for the backupd file
+timestamp=$(date "+%Y%m%d_%H%M%S")
+backup_filename="$backup_directory/$timestamp.tar.gz"
+
+#Create compressed tar archive
+tar czf "$backup_filename" "$source_directory"
+
+#check id the backup was successful
+if [ $? -eq 0 ]; then 
+echo "backup successed ..archived saved as :$backup_filename"
+else
+echo "error :backup failed"
+fi
+
